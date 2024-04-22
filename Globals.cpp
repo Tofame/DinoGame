2ac33f982aa64c9c13871df397e3aae5ac2e2f10
Tofame <niewiadoma.65@gmail.com>
2024-04-22 22:50:07 +0200

    Restructurize Globals
    Better order, comments descripting it.

--- Globals.cpp
+++ Globals.cpp
@@ -1,16 +1,19 @@
 #include "Globals.h"
 #include <filesystem>
 
-extern std::string projectPath = std::filesystem::current_path().parent_path().string();
-
-extern sf::Sound sound = sf::Sound();
-extern sf::Texture textureDinoRun = sf::Texture();
-extern sf::Texture textureDinoWalk = sf::Texture();
-
-extern double defaultAnimSF = 50; // default animator single frame interval value
-
+// GENERAL
 sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
 extern sf::RenderWindow window = sf::RenderWindow(desktopMode, "Dino The Game");
 
+extern std::string projectPath = std::filesystem::current_path().parent_path().string();
+
+// SYSTEM VARIABLES
+extern double defaultAnimSF = 50; // default animator single frame interval value
+
 extern const float gravity = 1.05;
 extern const float dash = 1.7;
+
+// STORED LOADED ASSETS
+extern sf::Sound sound = sf::Sound();
+extern sf::Texture textureDinoRun = sf::Texture();
+extern sf::Texture textureDinoWalk = sf::Texture();
--- Globals.h
+++ Globals.h
@@ -1,16 +1,20 @@
 #include "SFML/Audio.hpp"
 #include "SFML/Graphics.hpp"
 
+#include <thread>
+
+// GENERAL
+extern sf::RenderWindow window;
 
 extern std::string projectPath;
 
-extern sf::Sound sound;
-extern sf::Texture textureDinoRun;
-extern sf::Texture textureDinoWalk;
-
+// SYSTEM VARIABLES
 extern double defaultAnimSF;
-
-extern sf::RenderWindow window;
 
 extern const float gravity;
 extern const float dash;
+
+// STORED LOADED ASSETS
+extern sf::Sound sound;
+extern sf::Texture textureDinoRun;
+extern sf::Texture textureDinoWalk;
